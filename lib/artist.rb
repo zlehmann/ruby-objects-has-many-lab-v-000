679331a25ada3d34724a206aa0cae759c4fde44e
Zachary Lehmann <zlehmann@gmail.com>
2018-09-30 18:17:17 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,32 @@
+class Artist
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def name
+    @name
+  end
+
+  def songs
+    @songs
+  end
+
+  def add_song(song)
+    @songs << song
+    song.artist = self
+  end
+
+  def add_song_by_name(title)
+    song = Song.new(title)
+    @songs << song
+    song.artist = self
+  end
+
+  def self.song_count
+    Song.all.count
+  end
+
+end
